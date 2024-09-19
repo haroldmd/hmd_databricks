befe45ab92f0f00f1a103b72847221447f8dea23
haroldmd <harold.md@gmail.com>
2024-09-19 09:55:07 -0400

    high-volume days written to file

--- MarketAnalysis/Stock Data Analysis.py
+++ MarketAnalysis/Stock Data Analysis.py
@@ -16,4 +16,9 @@ df.select(max("Adj_Close"), max("Volume"))\
 
 # COMMAND ----------
 
+df.select("Date", "Adj_Close", "Volume")\
+  .where(df.Volume > 150000000)\
+  .write.option("header","true")\
+        .mode('overwrite')\
+        .csv("dbfs:/FileStore/outputs/tesla_highvoldays.csv")
 
